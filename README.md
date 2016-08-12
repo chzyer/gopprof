1d80cf23fb7f19a10162412e74ffc23e3d6caf37
chzyer <0@0xdf.com>
2016-08-12 22:27:10 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # gopprof
 A go pprof wrapper which provided full readline user experience
+
+### Get
+```
+$ go get github.com/chzyer/gopprof
+$ gopprof [options] [binary] <profile source> ...
+# just like anything with "go tool pprof"
+```
+
+```
+$ gopprof http://localhost:6060/debug/pprof/heap
+Fetching profile from http://localhost:6060/debug/pprof/heap
+Saved profile in /Users/xxx/pprof/pprof.localhost:6060.inuse_objects.inuse_space.028.pb.gz
+Entering interactive mode (type "help" for commands)
+(gopprof) _
+```
---
+++ gopprof.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/chzyer/readline"
+)
+
+func checkGo() error {
+	cmd := exec.Command("go")
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+	// it's exit code should return 2
+	return nil
+}
+
+func runPprof(args []string) error {
+	newArgs := make([]string, 2+len(args))
+	copy(newArgs[2:], args)
+	newArgs[0] = "tool"
+	newArgs[1] = "pprof"
+	cmd := exec.Command("go", newArgs...)
+
+	pr, pw := io.Pipe()
+	cmd.Stderr = pw
+	cmd.Stdout = pw
+	pprofIn, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	_ = pprofIn
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	errChan := make(chan error)
+	go func() {
+		errChan <- cmd.Wait()
+	}()
+	go process(pr, pprofIn)
+
+	return <-errChan
+}
+
+func process(pr io.ReadCloser, pprofIn io.Writer) {
+	r := bufio.NewReader(pr)
+	defer pr.Close()
+
+	rl := newrl()
+	defer rl.Close()
+	buffer := bytes.NewBuffer(nil)
+
+	for {
+		line, err := lineOrEOF(r, buffer)
+		if err != nil {
+			break
+		}
+		if line != "(pprof) " {
+			os.Stdout.Write([]byte(line))
+			continue
+		}
+
+	reread:
+		ret := rl.Line()
+		if ret.CanBreak() {
+			pprofIn.Write([]byte("quit\n"))
+			break
+		} else if ret.CanContinue() {
+			goto reread
+		}
+
+		pprofIn.Write([]byte(ret.Line))
+		pprofIn.Write([]byte("\n"))
+	}
+
+}
+
+func lineOrEOF(r *bufio.Reader, buf *bytes.Buffer) (string, error) {
+	buf.Reset()
+	for {
+		c, err := r.ReadByte()
+		if err != nil {
+			break
+		}
+		buf.WriteByte(c)
+		if c == '\n' {
+			break
+		}
+		if bytes.Equal(buf.Bytes(), []byte("(pprof) ")) {
+			break
+		}
+	}
+	return buf.String(), nil
+}
+
+func newrl() *readline.Instance {
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt: "(gopprof) ",
+	})
+	if err != nil {
+		panic(err)
+	}
+	return rl
+}
+
+func main() {
+	if err := checkGo(); err != nil {
+		println(err.Error())
+		os.Exit(2)
+	}
+
+	if err := runPprof(os.Args[1:]); err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+}
